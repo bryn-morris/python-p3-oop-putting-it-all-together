2efe17772beff439aea671a5ac7076f29c04e3b5
Brynfor Morris <brynfor.morris@gmail.com>
2023-03-15 16:04:00 -0500

    Passed OOP deliverables regarding property, attributes, and methods!

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+     
+    def __init__(self, title = "sample", page_count = 0):
+        self.title = title
+        self.page_count = page_count
+
+    def get_page(self):
+        return self._page_count
+    
+    def set_page(self,page_count):
+        if(type(page_count) == int):
+            self._page_count = page_count    
+        else:
+            print("page_count must be an integer")
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+                
+
+    page_count = property(get_page, set_page)
+
+sample_book = Book("And Then There Were None")
+print(sample_book.title)
+
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    def __init__(self, brand="sample", color = "blank", size = 0):
+        self.brand = brand
+        self.color = color
+        self.size = size
+
+    def size_getter(self):
+        return self._size
+
+    def size_setter(self,size):
+        print(size)
+        if(type(size) != int):
+            print("size must be an integer")
+        else:
+            self._size = size
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+    size = property(size_getter,size_setter)
+
+stan_smith = Shoe("Adidas")
+stan_smith.size = "not an integer"
+
